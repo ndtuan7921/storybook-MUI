499185975c497797e3e7626b89c1eadf58464329
ndtuan7921 <ndtuan7921@gmail.com>
2023-06-07 09:03:08 +0700

    init src and config packages

---
+++ src/components/TextButton/index.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import TextButton from ".";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: "Atoms/Button",
+  component: TextButton,
+  tags: ["autodocs"],
+} satisfies Meta<typeof TextButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    label: "Button",
+  },
+};
